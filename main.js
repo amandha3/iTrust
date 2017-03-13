f85c8a62c3853b5440f09659e73dee7cad50b909
Priyaranjan <pbehera@ncsu.edu>
2017-03-13 15:58:48 -0400

    Modified to get all test data in a single object. Looped mvn test for 2 times, can be changed for multiple times.

--- main.js
+++ main.js
@@ -39,88 +39,124 @@ function compare(a,b)
                     return a.time-b.time;
                 }
 
-try{
-//    child.execSync('cd simplecalc; mvn test');
-}
-catch(e)
+
+testResults = {};
+
+
+for(k=0; k<2; k++)
 {
-    //console.log(e);
-}
-
-
-list = [];
-
-filelist = [];
-
-filelist = walkSync(__dirname+'/target/surefire-reports',filelist);
-
-
-for( i=0; i < filelist.length; i++)
-{   
-//console.log("i: " + filelist[i])
-    var filename = filelist[i];
-    if(filename.endsWith('.xml'))
-    {
-
-	   console.log("Reading File: ", filename);
-       data = fs.readFileSync(filename).toString();
-       console.log("Reading File Content: ", data);
-       
-       var options = {
-            object: true,
-            reversible: false,
-            coerce: false,
-            sanitize: true,
-            trim: true,
-            arrayNotation: false,
-            alternateTextNode: false
-        };
-
-        result = parser.toJson(data, options);
-       console.log("Reading File Content Parsed: ", result);
-            
-
-            for(key in result)
-            {
-                console.log("Key: ", result[key]);
-            }
-            //console.log(result.testsuite.testcase[0]);
-            // Print out everything
-            //console.dir(JSON.stringify(result,null, 3));
-
-            
-
-            for(var testCase in result.testsuite.testcase)
-            {
-                var t = {};
-                //console.log(result.testsuite.testcase[testCase]);
-                t.classname = result.testsuite.testcase[testCase].classname;
-                t.testname = result.testsuite.testcase[testCase].name;
-                t.failed = false;;
-                t.time = parseFloat(result.testsuite.testcase[testCase].time);
-                if(result.testsuite.testcase[testCase].hasOwnProperty("failure"))
-                {
-                    console.log("Failed Test Detected:", result.testsuite.testcase[testCase].name, result.testsuite.testcase[testCase].classname);
-                    t.failed = true;
-                    //map.set();.
-
-                }
-                //map.set(testCase.)
-		//console.log("Pushing to List: ", t);
-                list.push(t);
-                console.log("Inner: ", Date.now());
-		//console.log("List length: ", list.length);
-            }
-
+   try{
+        //child.execSync('mvn test');
     }
+    catch(e)
+    {
+        console.log(e);
+    }
+
+
+
+
+
+    list = [];
+
+    filelist = [];
+
+    filelist = walkSync(__dirname+'/target/surefire-reports',filelist);
+
+
+    for( i=0; i < filelist.length; i++)
+    {   
+    //console.log("i: " + filelist[i])
+        var filename = filelist[i];
+        if(filename.endsWith('.xml'))
+        {
+
+           console.log("Reading File: ", filename);
+           data = fs.readFileSync(filename).toString();
+           console.log("Reading File Content: ", data);
+           
+           var options = {
+                object: true,
+                reversible: false,
+                coerce: false,
+                sanitize: true,
+                trim: true,
+                arrayNotation: false,
+                alternateTextNode: false
+            };
+
+            result = parser.toJson(data, options);
+           console.log("Reading File Content Parsed: ", result);
+                
+
+                for(key in result)
+                {
+                    console.log("Key: ", result[key]);
+                }
+                //console.log(result.testsuite.testcase[0]);
+                // Print out everything
+                //console.dir(JSON.stringify(result,null, 3));
+
+                
+
+                for(var testCase in result.testsuite.testcase)
+                {
+                    var t = {};
+                    //console.log(result.testsuite.testcase[testCase]);
+                    t.classname = result.testsuite.testcase[testCase].classname;
+                    t.testname = result.testsuite.testcase[testCase].name;
+                    t.failed = false;;
+                    t.time = parseFloat(result.testsuite.testcase[testCase].time);
+                    if(result.testsuite.testcase[testCase].hasOwnProperty("failure"))
+                    {
+                        console.log("Failed Test Detected:", result.testsuite.testcase[testCase].name, result.testsuite.testcase[testCase].classname);
+                        t.failed = true;
+                        //map.set();.
+
+                    }
+                    //map.set(testCase.)
+            //console.log("Pushing to List: ", t);
+                    list.push(t);
+                    var keyName = t.classname+":"+t.testname;
+                    if(keyName in testResults)
+                    {
+                        if(t.failed)
+                            testResults[keyName].failure++;
+                        else
+                            testResults[keyName].success++;
+                    }
+                    else
+                    {
+
+                        var temp = {
+                              success: 0,
+                              failure: 0,
+                            };
+
+                        testResults[keyName] = temp;
+
+                        if(t.failed)
+                            testResults[keyName].failure++;
+                        else
+                            testResults[keyName].success++;
+                    }
+                    console.log("Inner: ", Date.now());
+            //console.log("List length: ", list.length);
+                }
+
+        }
+    }
+
+
+    console.log("Comparing:");
+
+    list.sort(compare);
+
+    console.log("Inner: ", Date.now());
+    console.log('Done');
+
+    console.log("List: ", list.length); 
 }
 
 
-console.log("Comparing:");
-
-list.sort(compare);
-
-console.log("Inner: ", Date.now());
-console.log('Done');
-
-console.log("List: ", list.length);
+console.log("Dict is: ", testResults);
