ce0ee2209ebfe978d3164b04b631105bcde5aa5f
Priyaranjan <pbehera@ncsu.edu>
2017-03-13 21:37:42 -0400

    Merged all js files

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var fs = require('fs'),
     parser = require('xml2json'),
-    child  = require('child_process'); 
-    fuzzing = require("./fuzzing.js");
+    child  = require('child_process');
 //var parser = new xml2js.Parser();
 
 
@@ -40,6 +39,171 @@ function compare(a,b)
                 }
 
 
+
+
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+var fuzzer = 
+{
+    random : new Random(Random.engines.mt19937().seed(0)),
+    
+    seed: function (kernel)
+    {
+        fuzzer.random = new Random(Random.engines.mt19937().seed(kernel));
+    },
+
+    mutate:
+    {
+        string: function(val)
+        {
+            // MUTATE IMPLEMENTATION HERE
+
+
+            var array = val.split(' ');
+
+            for(i=0; i< array.length; i++)
+            {
+                if(array[i].indexOf('"') != -1)
+                {
+                    var fIndex = array[i].indexOf('"');
+                    if(array[i].indexOf('"', fIndex+1) != -1)
+                    {
+                        sIndex = array[i].indexOf('"', fIndex+1);
+                        array[i] = array[i].substring(0,fIndex+1)+faker.random.word()+array[i].substring(sIndex);
+                    }
+                }
+            }
+
+            var mergedCode = array.join(' ');
+
+
+            array = mergedCode.split('');
+
+            for(i=0; i<array.length; i++)
+            {
+                if(array[i] == '<' && array[i-1] == ' ')
+                {
+                    array[i] = '>';
+                }
+            }
+
+            mergedCode =  array.join('');
+
+            return mergedCode;
+        }
+    }
+};
+
+fuzzer.seed(10);
+
+function mutateFile(filename)
+{
+    console.log("Mutating File", filename);
+    var fileContent = fs.readFileSync(filename,'utf-8');
+    //var markDown = fs.readFileSync('simple.md','utf-8');
+
+    var mutuatedString = fuzzer.mutate.string(fileContent);
+    console.log("Mutated String: ", mutuatedString);
+
+    return mutuatedString;
+}
+
+function mutationTesting()
+{
+    var markDown = fs.readFileSync('test.md','utf-8');
+    //var markDown = fs.readFileSync('simple.md','utf-8');
+
+    var mutuatedString = fuzzer.mutate.string(markDown);
+
+    console.log("Mutated String: ", mutuatedString);
+      
+}
+
+
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var walkSync = function(dir, filelist) {
+            var path = path || require('path');
+            var fs = fs || require('fs'),
+                files = fs.readdirSync(dir);
+            filelist = filelist || [];
+            files.forEach(function(file) {
+                if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                    filelist = walkSync(path.join(dir, file), filelist);
+                }
+                else {
+                    filelist.push(path.join(dir, file));
+                }
+            });
+            return filelist;
+        };
+
+
+
+mutateAFile = function() {
+    filelist = [];
+
+    filelist = walkSync(__dirname+'/src/main/edu/ncsu/csc/itrust',filelist);
+
+    //fileIndex = getRandomArbitrary(0,filelist.length);
+
+    fileIndex = 82;
+
+    console.log("FileIndex: ", fileIndex);
+
+    mutatedString = mutateFile(filelist[fileIndex]);
+
+    console.log("FileName: ", filelist[fileIndex]);
+    console.log("Mutated String". mutatedString);
+
+    fs.writeFileSync(filelist[fileIndex], mutatedString, 'utf8');
+};
+
+
+
+
+//mutationTesting();
+
+//mutationTesting();
+
+//test('markedMutation', function(t) {
+//
+//});
+
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 testResults = {};
 
 
@@ -47,15 +211,17 @@ for(k=0; k<2; k++)
 {
    try{
 
-        fuzzing.mutateAFile();
-        child.execSync('mvn test');
-
+        mutateAFile();
+        console.log("Before Exec Sync");
+        //child.execSync('mvn test');
+        console.log("After Exec Sync");
     list = [];
 
     filelist = [];
 
     filelist = walkSync(__dirname+'/target/surefire-reports',filelist);
 
+    console.log("FileList: ", filelist);
 
     for( i=0; i < filelist.length; i++)
     {   
